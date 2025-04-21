e68379c3eaba022db5d729c0110db4e2a44cb689
Thiago7000 <tiagorufinoh2@gmail.com>
2025-04-21 23:05:07 +0000

    testei algo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,32 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    printf ("desafio super trunfo jogos");
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+//cidade 1
+    char estado1;
+    char codigo_da_carta1[3];
+    char nome_da_cidade1 [30];
+    int populacao1= 3000;
+    float area1= 12.500;
+    float PIB1= 20.000;
+    int quantidade_de_pontos_turisticos1= 12;
 
-    return 0;
+//cidade 2
+    char estado2;
+    char codigo_da_carta2[3];
+    char nome_da_cidade2 [30];
+    int populacao2= 300;
+    float area2= 2000.22;
+    float PIB2= 100.00;
+    int quantidade_de_pontos_turisticos2= 3 ;
+
+    printf ("Bem vindo ao desafio super trunfo do Thiago\n");
+    printf ("o objetivo do jogo e que voce crie 2 cartas, vamos comecar criando a primeira\n");
+
+//pedindo e recebendo as caracteriscas da primeira carta
+printf ("digite uma letra de A a H\n");
+scanf (" %c\n", &estado1);
+printf ("letra: %c\n", estado1);
+
+return 0;
+
 }
